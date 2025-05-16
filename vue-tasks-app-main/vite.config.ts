12f946d6d5404a16fd0520d4766f4f1187fce987
ged <ged1959@users.noreply.github.com>
2025-05-16 22:17:32 +0900

    second commit

---
+++ vue-tasks-app-main/vite.config.ts
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+// https://vite.dev/config/
+export default defineConfig({
+  base: '/hogeToDoApp0516/', // これ、つまりレポジトリ名を追記する。
+  plugins: [vue()],
+})
